731579e20b8f9cf8cf6b97faaa50e40b96c16def
Administrator <2233250059@qq.com>
2018-04-15 15:24:08 +0800

    一点点

--- js/1.js
+++ js/1.js
@@ -4,14 +4,3 @@
  */
 var num =window.prompt("请输入一个年龄","18");
 console.log("您已经活了"+num*365*24*60*60+"秒");
-
-var aa=function () {
-    console.log(hhh);
-}
-aa();/*只能在下面调用*/
-function bb() {
-    console.log(jjj);
-}
-bb();/*上下都可以 函数提升*/
-console.log(cc);
-var cc=4;/*变量提升 undefined*/
