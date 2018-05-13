a65ac05989903ef43ea85c4ff1ec7050d27cf5e2
Administrator <2233250059@qq.com>
2018-05-13 09:21:13 +0800

    一点点

---
+++ jQuery/jQuery练习/2_表格/1-表格变色/demo1.js
@@ -0,0 +1,7 @@
+	$(function(){
+		$("tbody>tr:odd").addClass("odd"); //先排除第一行,然后给奇数行添加样式
+		$("tbody>tr:even").addClass("even"); //先排除第一行,然后给偶数行添加样式
+        $(".odd,.even").on("click",function () {
+            $(this).toggleClass("selected");
+        });
+	})
