0ca69bd7dd6c1a764be68943dd08870627ab5448
Administrator <2233250059@qq.com>
2018-06-01 19:45:39 +0800

    一点点

--- 菜单/04-特效菜单/js.js
+++ 菜单/04-特效菜单/js.js
@@ -1,32 +1,27 @@
 
-/**
- * Created by Administrator on 2018/5/28 0028.
- */
-    $(function () {
-    //navlist
-    $('#navList').on('click', '.nav-btn', function (event) {
-        if ($(this).hasClass('btn-active')) {
-            $('#expandZone #closeBtn').click();
-            return false;
-        }
-        var curIndex = $(this).index() , mlValue = '-' + curIndex * 100 + '%';
-        if ($('#expandZone').hasClass('active')) {
-            $('#expandZone .expdiv').animate({ marginLeft: mlValue });
-        }
-        else {
-            $('#expandZone .expdiv').css({ marginLeft: mlValue });
+$('.nav-btn').on('click', function () {
+    var iNow=$(this).index();
+    var marginLeft='-'+iNow*100+'%';//显示对应二级菜单
 
-            $('#expandZone').animate({ height: '130px' }).addClass('active');
-        }
-        $(this).addClass('btn-active').siblings().removeClass('btn-active');
-        return false;
+    if ($(this).hasClass('btn-active')){
+        $('#closeBtn').trigger('click');//触发点击事件
+        // $('#closeBtn').click();//查找点击事件
+    }
+    if ($(".expand").hasClass('active')) {
+        $('.expdiv').animate({marginLeft:marginLeft});//激活状态 动画
+    }
+    else {   //不激活状态 直接设置css
+        $('.expdiv').css('marginLeft',marginLeft);
+        $('.expand').animate({
+            height:130
+        }).addClass('active');
+    }
+    $(this).addClass('btn-active').siblings().removeClass('btn-active');//添加类判断是否当前a为激活状态 激活状态再次点击收起二级菜单
+});
+$('#closeBtn').on('click',function () {
+    $('.expand').animate({
+        height:0
+    },function () {
+       $(this).removeClass('active');//移除class 下次点击一级菜单执行if语句
     });
-
-    $('#expandZone #closeBtn').on('click', function () {
-    $('#expandZone').animate({ height: '0px' }, function () {
-    $(this).removeClass('active');
-    $('#navList .btn-active').removeClass('btn-active');
-});
-    return false;
-});
 });
