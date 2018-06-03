a43969d4ced06280122442c6662fb97fff9d9ecb
Administrator <2233250059@qq.com>
2018-06-03 22:16:00 +0800

    一点点

--- jQuery/练习/7_网上商城/scripts/use_jqzoom.js
+++ jQuery/练习/7_网上商城/scripts/use_jqzoom.js
@@ -1,14 +1,14 @@
 ﻿/*使用jqzoom*/
 $(function(){
 	$('.jqzoom').jqzoom({
-		zoomType: 'standard',
-		lens:true,
-		preloadImages: false,
-		alwaysOn:false,
-		zoomWidth: 340,
+		zoomType: 'standard',/*遮罩层样式*/
+		lens:true,/*鼠标滑入显示遮罩层*/
+		preloadImages: false,/*预加载*/
+		alwaysOn:false,/*初始是否显示遮罩层*/
+		zoomWidth: 340,/*放大图片的宽高*/
 		zoomHeight: 340,
-		xOffset:10,
+		xOffset:10,/*放大图片距离原始图片距离*/
 		yOffset:0,
-		position:'right'
+		position:'right'/*放大图片位置*/
     });
 });
