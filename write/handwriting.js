1fd9d1af164cae88d7b6ba9c06da65f8523f63a7
Administrator <2233250059@qq.com>
2018-06-30 15:03:13 +0800

    一点点

--- write/handwriting.js
+++ write/handwriting.js
@@ -1,51 +1,51 @@
-var canvasWidth = Math.min( 800 , $(window).width() - 20 )
-var canvasHeight = canvasWidth
+var canvasWidth = Math.min( 800 , $(window).width() - 20 );
+var canvasHeight = canvasWidth;
 
-var strokeColor = "black"
-var isMouseDown = false
-var lastLoc = {x:0,y:0}
-var lastTimestamp = 0
-var lastLineWidth = -1
+var strokeColor = "black";
+var isMouseDown = false;
+var lastLoc = {x:0,y:0};
+var lastTimestamp = 0;
+var lastLineWidth = -1;
 
-var canvas = document.getElementById("canvas")
-var context = canvas.getContext("2d")
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 
-canvas.width = canvasWidth
-canvas.height = canvasHeight
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
 
-$("#controller").css("width",canvasWidth+"px")
-drawGrid()
+$("#controller").css("width",canvasWidth+"px");
+drawGrid();
 
 $("#clear_btn").click(
-    function(e){
-        context.clearRect( 0 , 0 , canvasWidth, canvasHeight )
-        drawGrid()
+    function(){
+        context.clearRect( 0 , 0 , canvasWidth, canvasHeight );/*清空一个指定矩形 x,y,width,height*/
+        drawGrid();
     }
-)
+);
 $(".color_btn").click(
-    function(e){
-        $(".color_btn").removeClass("color_btn_selected")
-        $(this).addClass("color_btn_selected")
-        strokeColor = $(this).css("background-color")
+    function(){
+        $(".color_btn").removeClass("color_btn_selected");
+        $(this).addClass("color_btn_selected");
+        strokeColor = $(this).css("background-color");
     }
-)
+);
 
 function beginStroke(point){
 
-    isMouseDown = true
+    isMouseDown = true;
     //console.log("mouse down!")
-    lastLoc = windowToCanvas(point.x, point.y)
+    lastLoc = windowToCanvas(point.x, point.y);
     lastTimestamp = new Date().getTime();
 }
 function endStroke(){
-    isMouseDown = false
+    isMouseDown = false;
 }
 function moveStroke(point){
 
     var curLoc = windowToCanvas( point.x , point.y );
     var curTimestamp = new Date().getTime();
-    var s = calcDistance( curLoc , lastLoc )
-    var t = curTimestamp - lastTimestamp
+    var s = calcDistance( curLoc , lastLoc );
+    var t = curTimestamp - lastTimestamp;
 
     var lineWidth = calcLineWidth( t , s );
 
@@ -54,31 +54,31 @@ function moveStroke(point){
     context.moveTo( lastLoc.x , lastLoc.y );
     context.lineTo( curLoc.x , curLoc.y );
 
-    context.strokeStyle = strokeColor
-    context.lineWidth = lineWidth
-    context.lineCap = "round"
-    context.lineJoin = "round"
-    context.stroke()
+    context.strokeStyle = strokeColor;
+    context.lineWidth = lineWidth;
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.stroke();
 
-    lastLoc = curLoc
-    lastTimestamp = curTimestamp
-    lastLineWidth = lineWidth
+    lastLoc = curLoc;
+    lastTimestamp = curTimestamp;
+    lastLineWidth = lineWidth;
 }
 
 canvas.onmousedown = function(e){
-    e.preventDefault()
+    e.preventDefault();
     beginStroke( {x: e.clientX , y: e.clientY} )
 };
 canvas.onmouseup = function(e){
-    e.preventDefault()
+    e.preventDefault();
     endStroke()
 };
 canvas.onmouseout = function(e){
-    e.preventDefault()
+    e.preventDefault();
     endStroke()
 };
 canvas.onmousemove = function(e){
-    e.preventDefault()
+    e.preventDefault();
     if( isMouseDown ){
         moveStroke({x: e.clientX , y: e.clientY})
     }
@@ -97,7 +97,7 @@ canvas.addEventListener('touchmove',function(e){
     }
 });
 canvas.addEventListener('touchend',function(e){
-    e.preventDefault()
+    e.preventDefault();
     endStroke()
 });
 
@@ -126,43 +126,43 @@ function calcLineWidth( t , s ){
 
 function calcDistance( loc1 , loc2 ){
 
-    return Math.sqrt( (loc1.x - loc2.x)*(loc1.x - loc2.x) + (loc1.y - loc2.y)*(loc1.y - loc2.y) )
+    return Math.sqrt( (loc1.x - loc2.x)*(loc1.x - loc2.x) + (loc1.y - loc2.y)*(loc1.y - loc2.y) );
 }
 
 function windowToCanvas( x , y ){
-    var bbox = canvas.getBoundingClientRect()
+    var bbox = canvas.getBoundingClientRect();
     return {x:Math.round(x-bbox.left) , y:Math.round(y-bbox.top)}
 }
 function drawGrid(){
 
-    context.save()
+    context.save();
 
-    context.strokeStyle = "rgb(230,11,9)"
+    context.strokeStyle = "rgb(230,11,9)";
 
-    context.beginPath()
-    context.moveTo( 3 , 3 )
-    context.lineTo( canvasWidth - 3 , 3 )
-    context.lineTo( canvasWidth - 3 , canvasHeight - 3 )
-    context.lineTo( 3 , canvasHeight - 3 )
-    context.closePath()
-    context.lineWidth = 6
-    context.stroke()
+    context.beginPath();
+    context.moveTo( 3 , 3 );
+    context.lineTo( canvasWidth - 3 , 3 );
+    context.lineTo( canvasWidth - 3 , canvasHeight - 3 );
+    context.lineTo( 3 , canvasHeight - 3 );
+    context.closePath();
+    context.lineWidth = 6;
+    context.stroke();
 
-    context.beginPath()
-    context.moveTo(0,0)
-    context.lineTo(canvasWidth,canvasHeight)
+    context.beginPath();
+    context.moveTo(0,0);
+    context.lineTo(canvasWidth,canvasHeight);
 
-    context.moveTo(canvasWidth,0)
-    context.lineTo(0,canvasHeight)
+    context.moveTo(canvasWidth,0);
+    context.lineTo(0,canvasHeight);
 
-    context.moveTo(canvasWidth/2,0)
-    context.lineTo(canvasWidth/2,canvasHeight)
+    context.moveTo(canvasWidth/2,0);
+    context.lineTo(canvasWidth/2,canvasHeight);
 
-    context.moveTo(0,canvasHeight/2)
-    context.lineTo(canvasWidth,canvasHeight/2)
+    context.moveTo(0,canvasHeight/2);
+    context.lineTo(canvasWidth,canvasHeight/2);
 
-    context.lineWidth = 1
-    context.stroke()
+    context.lineWidth = 1;
+    context.stroke();
 
     context.restore()
 }
