265219d6ea009be273a8f0cfffc0dac17beb142c
huahuazuimeini <2233250059@qq.com>
2018-07-25 23:48:20 +0800

    master

--- 全屏插件/pageswitch.js
+++ 全屏插件/pageswitch.js
@@ -227,7 +227,7 @@
 		index : 0,		//页面开始的索引
 		easing : "ease",		//动画效果
 		duration : 500,		//动画执行时间
-		loop : true,		//是否循环切换
+		loop : false,		//是否循环切换
 		pagination : true,		//是否进行分页
 		keyboard : true,		//是否触发键盘事件
 		direction : "vertical",		//滑动方向vertical,horizontal
