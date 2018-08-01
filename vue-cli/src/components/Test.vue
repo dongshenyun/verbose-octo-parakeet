bf68a40e59b11ef64558012e0cfc43ed099bfd6f
huahuazuimeini <2233250059@qq.com>
2018-08-01 22:22:08 +0800

    master

---
+++ vue-cli/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    
+        <h2>huahua</h2>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+    
+    
+    
+    }
+</script>
+
+<style scoped>
+    h2 {
+    
+        color: #64ca20;
+    
+    }
+</style>
